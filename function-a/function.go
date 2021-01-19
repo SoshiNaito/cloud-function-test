9bf2ebb9b78fe9ab62e81cb66a1342e5ea31e198
内藤 綜志 <naito.soshi@gmail.com>
2021-01-19 15:56:35 +0900

    first commit

---
+++ function-a/function.go
@@ -0,0 +1,64 @@
+// Package p contains an HTTP Cloud Function.
+package p
+
+import (
+	"encoding/json"
+	"fmt"
+	"html"
+	"io"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+type environment struct {
+	env    string
+	db     string
+	dbUser string
+	dbPass string
+}
+
+var config *environment
+
+func init() {
+	err := godotenv.Load(fmt.Sprintf("../%s.env", os.Getenv("GO_ENV")))
+	if err != nil {
+		// .env読めなかった場合の処理
+	}
+	config = &environment{
+		os.Getenv("ENV"),
+		os.Getenv("DB"),
+		os.Getenv("DB_USER"),
+		os.Getenv("DB_PASS"),
+	}
+}
+
+func Main(w http.ResponseWriter, r *http.Request) {
+	var d struct {
+		Message string `json:"message"`
+	}
+
+	if err := json.NewDecoder(r.Body).Decode(&d); err != nil {
+		switch err {
+		case io.EOF:
+			fmt.Fprint(w, "Hello World!")
+			fmt.Println(config.env)
+			fmt.Println(config.db)
+			fmt.Println(config.dbUser)
+			fmt.Println(config.dbPass)
+			return
+		default:
+			log.Printf("json.NewDecoder: %v", err)
+			http.Error(w, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
+			return
+		}
+	}
+
+	if d.Message == "" {
+		fmt.Fprint(w, "Hello World!")
+		return
+	}
+	fmt.Fprint(w, html.EscapeString(d.Message))
+}
---
+++ function-a/go.mod
@@ -0,0 +1,5 @@
+module example.com/cloudfunction
+
+go 1.15
+
+require github.com/joho/godotenv v1.3.0
